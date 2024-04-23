4c8eab95ef6c8c10af27a01c61ad00b994cae589
JangGwon <tlsakch510@naver.com>
2024-04-23 18:12:21 +0900

    로그인 기능 구현

--- myapp/src/app/login/page.tsx
+++ myapp/src/app/login/page.tsx
@@ -1,17 +1,52 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const idd = "test";
+const pww = "pw1234";
+
 export default function Login() {
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+
+  const router = useRouter();
+
+  function onClickLoginButton() {
+    if (id === idd && pw === pww) {
+      router.push("/main");
+    } else {
+      alert("아이디 또는 비밀번호가 올바르지 않습니다.");
+    }
+  }
+
   return (
-    <main className="flex min-w-[360px] max-w-[600px] mx-auto h-screen bg-white">
-      <div>
-        <div>
-          <label className="w-[10rem] text-center">아이디 </label>
-          <input className="w-[10rem] h-[2rem] border-black border-2" />
-        </div>
-        <div>
-          <label className="w-[10rem]">비밀번호 </label>
-          <input className="w-[10rem] h-[2rem] border-black border-2" />
+    <main className="min-w-[360px] max-w-[600px] mx-auto h-[720px] bg-white">
+      <div className="w-[300px] h-[200px]  mx-auto  border-2 border-gray-300 rounded-lg">
+        <div className="h-fit w-fit mx-auto mt-[2rem] ">
+          <div className="flex w-[240px]">
+            <label className="w-[10rem] text-center">아이디 </label>
+            <input
+              onChange={(event) => setId(event.target.value)}
+              className="w-[10rem] h-[2rem] border-black border-2"
+            />
+          </div>
+          <div className="flex w-[240px] my-1">
+            <label className="w-[10rem] text-center">비밀번호 </label>
+            <input
+              type="password"
+              onChange={(event) => setPw(event.target.value)}
+              className="w-[10rem] h-[2rem] border-black border-2"
+            />
+          </div>
+          <button
+            onClick={onClickLoginButton}
+            className="w-[15rem] h-[2rem] mt-4 mx-auto border-2 border-black rounded-lg "
+          >
+            로그인
+          </button>
         </div>
       </div>
-      <button>로그인</button>
     </main>
   );
 }
--- myapp/src/app/page.tsx
+++ myapp/src/app/page.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/login");
+  }, []);
   return (
-    <main className="flex min-w-[360px] max-w-[600px] mx-auto h-screen bg-white">
-      <input className="w-[10rem] h-[2rem]" />
-      <input className="w-[10rem] h-[2rem]" />
-      <button>로그인</button>
-    </main>
+    <main className="flex min-w-[360px] max-w-[600px] mx-auto h-screen bg-white"></main>
   );
 }
